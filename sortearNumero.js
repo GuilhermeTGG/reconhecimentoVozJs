c196d6bcd7dc42708dad9b82fc697b514b2ad2e0
GuilhermeTGG <guilherme050592@gmail.com>
2022-11-10 07:43:53 -0300

    sorteandoNumeroOk

--- sortearNumero.js
+++ sortearNumero.js
@@ -1,1 +1,14 @@
-console.log("chupa")
+const maiorValor = 1000
+const menorValor = 10
+
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor
